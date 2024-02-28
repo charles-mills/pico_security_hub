683f39b777711dd4c6b8b719008e59f2a31849cd
Charlie Mills <charlesrjmills@gmail.com>
2024-02-28 22:13:21 +0000

    Refactor motion detection script

--- sensors/motion_detection.py
+++ sensors/motion_detection.py
@@ -9,39 +9,68 @@ from pico_security_hub.config import publishing
 from pico_security_hub.controllers import control_led
 from pico_security_hub.controllers import control_buzzer
 
+HIGH_SENSITIVE_FLAGS = 2
+LOW_SENSITIVE_FLAGS = 4
+
 motion_detected = False
 expected_range_cm = 0
 sonic_ranger = pulseio.PulseIn(board.GP3)
 
 
-async def get_baseline(loops):
+def check_motion_sensitive():
     """
-    This function calculates the baseline distance for the motion detection.
-
-    :param loops: The number of measurements to take.
-    :return centre_result: The median (not yet sorted) distance of the measurements.
-
+    This function checks if the motion detection is set to be sensitive or not.
     """
+    if master.config_dict["motion_sensitive"]:
+        return HIGH_SENSITIVE_FLAGS
+    return LOW_SENSITIVE_FLAGS
+
+
+async def handle_motion_publishing(motion_detection_flags, allowed_flags):
+    if motion_detection_flags >= allowed_flags:
+        motion_detection_flags = 0
+        if master.config_dict["motion_publish"]:
+            networking.publ_data(
+                networking.mqtt_link, "motion_detected", "Motion Detected!", mute=True)
+            await publishing.trigger("Motion Detection", "Motion Detected")
+        else:
+            await publishing.trigger("Motion Detection", "Motion Detected", False)
+    return motion_detection_flags
+
+
+def handle_motion_detection(actual_dist_cm, motion_detection_flags):
     global motion_detected
 
-    master.config_dict["motion_enabled"] = False
-    results = []
+    if actual_dist_cm < expected_range_cm:
+        motion_detection_flags += 1
+        control_led.alarm_active = True
+        motion_detected = True
+        control_buzzer.queue.append("alarm")
+    elif actual_dist_cm > expected_range_cm and motion_detection_flags > 0:
+        motion_detection_flags -= 1
+        control_led.alarm_active = False
+        motion_detected = False
+    else:
+        motion_detection_flags = 0
+        control_led.alarm_active = False
+        motion_detected = False
+    return motion_detection_flags
 
-    for _ in range(loops):
-        sonic_ranger.clear()
-        sonic_ranger.resume(1)
-        await asyncio.sleep(0.1)
-        sonic_ranger.pause()
 
-        try:
-            results.append(sonic_ranger[0] * 0.017)
-        except IndexError:
-            pass
+async def measure_distance():
+    """
+    This function measures the distance using the sonic ranger sensor.
 
-    centre_result = results[int(loops / 2)]
-    print(f"Motion Detection: Baseline Test Completed - {centre_result}cm")
-    master.config_dict["motion_enabled"] = True
-    return centre_result
+    :return actual_dist_cm: The measured distance in centimeters.
+    """
+    sonic_ranger.clear()
+    sonic_ranger.resume(1)
+    await asyncio.sleep(0.1)
+    sonic_ranger.pause()
+
+    actual_dist_cm = sonic_ranger[0] * 0.017
+
+    return actual_dist_cm
 
 
 async def check_motion():
@@ -58,42 +87,36 @@ async def check_motion():
 
     while True:
         if master.MASTER_LOOP and master.config_dict["motion_enabled"]:
-            allowed_flags = 5
+            allowed_flags = check_motion_sensitive()
+            actual_dist_cm = await measure_distance()
+            motion_detection_flags = handle_motion_detection(actual_dist_cm, motion_detection_flags)
+            motion_detection_flags = await handle_motion_publishing(motion_detection_flags, allowed_flags)
+        await asyncio.sleep(0.5)
 
-            if master.config_dict["motion_sensitive"]:
-                allowed_flags = 2
 
-            sonic_ranger.clear()
-            sonic_ranger.resume(1)
-            await asyncio.sleep(0.1)
-            sonic_ranger.pause()
+async def get_baseline(loops):
+    """
+    This function calculates the baseline distance for the motion detection.
 
-            actual_dist_cm = sonic_ranger[0] * 0.017
+    :param loops: The number of measurements to take.
+    :return centre_result: The median (not yet sorted) distance of the measurements.
 
-            if actual_dist_cm < expected_range_cm:
-                motion_detection_flags += 1
-                control_led.alarm_active = True
-                motion_detected = True
-                control_buzzer.queue.append("alarm")
-            elif actual_dist_cm > expected_range_cm and motion_detection_flags > 0:
-                motion_detection_flags -= 1
-                control_led.alarm_active = False
-                motion_detected = False
-            else:
-                motion_detection_flags = 0
-                control_led.alarm_active = False
-                motion_detected = False
+    """
+    global motion_detected
 
-            if motion_detection_flags >= allowed_flags:
-                motion_detection_flags = 0
-                if master.config_dict["motion_publish"]:
-                    networking.publ_data(
-                        networking.mqtt_link, "motion_detected", "Motion Detected!", mute=True)
-                    await publishing.trigger("Motion Detection", "Motion Detected")
-                else:
-                    await publishing.trigger("Motion Detection", "Motion Detected", False)
-        else:
-            await asyncio.sleep(0.5)
+    master.config_dict["motion_enabled"] = False
+    results = []
+
+    for _ in range(loops):
+        try:
+            results.append(await measure_distance())
+        except IndexError:
+            pass
+
+    centre_result = results[int(loops / 2)]  # This calculation should be improved by calculating median
+    print(f"Motion Detection: Baseline Test Completed - {centre_result}cm")
+    master.config_dict["motion_enabled"] = True
+    return centre_result
 
 
 async def main():
